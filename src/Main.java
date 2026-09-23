a4dccddb0b37a6036999cbe7f372b66553437eff
krenmayr-droid <herbert.mutter@gmx.at>
2026-09-23 20:01:31 +0200

    pom.xml dazu

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,25 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
+import java.io.IOException;
+import java.util.List;
 
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
+public class Main {
+
+    public static void main(String[] args) {
+        try {
+            List<DiscountCode> discountCodes =
+                    DiscountCodeReader.readDiscountCodes();
+
+            for (DiscountCode discountCode : discountCodes) {
+                System.out.println(discountCode);
+            }
+
+            System.out.println(
+                    "Anzahl gelesener Rabattcodes: " + discountCodes.size()
+            );
+
+        } catch (IOException e) {
+            System.err.println(
+                    "Fehler beim Einlesen der Rabattcodes: " + e.getMessage()
+            );
+        }
     }
 }
